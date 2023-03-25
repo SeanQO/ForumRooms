bb461e5cb99f2cbce17d59b812c86d13d4e4b93f
SeanQO <seanquintero3@gmail.com>
2023-03-25 14:30:21 -0500

    Linked and expanded views to show actual model content

--- base/views.py
+++ base/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': "python"},
-    {'id': 2, 'name': "java"},
-    {'id': 3, 'name': "sql"},
-    {'id': 4, 'name': "django"}
-]
+from .models import *
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'home.html', context)
 
 
 def room(request, pk):
-    room_context = None
-
-    for r in rooms:
-        if r['id'] == int(pk):
-            room_context = r
-
-    context = {'room': room_context}
+    r = Room.objects.get(id=pk)
+    r = Room.objects.get(id=pk)
+    context = {
+        'room': r
+    }
     return render(request, 'room.html', context)
