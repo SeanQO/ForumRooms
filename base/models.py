2c28e876b4e377bded35488c2ec5598ddfeb934f
SeanQO <seanquintero3@gmail.com>
2023-03-25 05:34:46 -0500

    added topic

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return str(self.name)
